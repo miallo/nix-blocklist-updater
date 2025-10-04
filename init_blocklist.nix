c10b2e81524d321201b62220a5390a6cb3f703bd
Michael Lohmann <git@lohmann.sh>
2025-10-04 18:18:06 +0200

    refactor(init_blocklist): extract mkRules function

--- init_blocklist.nix
+++ init_blocklist.nix
@@ -1,4 +1,4 @@
-{ pkgs, config }:
+{ pkgs, config, mkRules }:
 let
   inherit (config.services.blocklist-updater) ipV4SetName ipV6SetName;
 in
@@ -14,14 +14,7 @@ in
       ipset create "${ipV4SetName}" hash:ip hashsize 262144
 
       # Blacklist all addresses from this ip set
-      iptables -I INPUT -m set --match-set ${ipV4SetName} src -j DROP
-      iptables -I INPUT -m set --match-set ${ipV4SetName} src -j LOG --log-prefix "FW_DROPPED: "
-
-      iptables -I FORWARD -m set --match-set ${ipV4SetName} src -j DROP
-      iptables -I FORWARD -m set --match-set ${ipV4SetName} src -j LOG --log-prefix "FW_DROPPED: "
-
-      iptables -t raw -I PREROUTING -m set --match-set ${ipV4SetName} src -j DROP
-      iptables -t raw -I PREROUTING -m set --match-set ${ipV4SetName} src -j LOG --log-prefix "FW_DROPPED: "
+      ${mkRules "iptables" "-I" ipV4SetName}
   fi
 
   if ! ipset -L ${ipV6SetName} >/dev/null 2>&1; then
@@ -30,13 +23,6 @@ in
       ipset create "${ipV6SetName}" hash:ip hashsize 262144 family inet6
 
       # Blacklist all addresses from this ip set
-      ip6tables -I INPUT -m set --match-set ${ipV6SetName} src -j DROP
-      ip6tables -I INPUT -m set --match-set ${ipV6SetName} src -j LOG --log-prefix "FW_DROPPED: "
-
-      ip6tables -I FORWARD -m set --match-set ${ipV6SetName} src -j DROP
-      ip6tables -I FORWARD -m set --match-set ${ipV6SetName} src -j LOG --log-prefix "FW_DROPPED: "
-
-      ip6tables -t raw -I PREROUTING -m set --match-set ${ipV6SetName} src -j DROP
-      ip6tables -t raw -I PREROUTING -m set --match-set ${ipV6SetName} src -j LOG --log-prefix "FW_DROPPED: "
+      ${mkRules "ip6tables" "-I" ipV6SetName}
   fi
 ''
--- service.nix
+++ service.nix
@@ -1,12 +1,22 @@
 { pkgs, config, ... }:
 let
   cfg = config.services.blocklist-updater;
+  mkRules = bin: f: set: ''
+    ${bin} ${f} INPUT -m set --match-set ${set} src -j DROP
+    ${bin} ${f} INPUT -m set --match-set ${set} src -j LOG --log-prefix "FW_DROPPED: "
+
+    ${bin} ${f} FORWARD -m set --match-set ${set} src -j DROP
+    ${bin} ${f} FORWARD -m set --match-set ${set} src -j LOG --log-prefix "FW_DROPPED: "
+
+    ${bin} -t raw ${f} PREROUTING -m set --match-set ${set} src -j DROP
+    ${bin} -t raw ${f} PREROUTING -m set --match-set ${set} src -j LOG --log-prefix "FW_DROPPED: "
+  '';
 in
 {
   systemd.services."blocklist" = {
     enable = cfg.enable;
     preStart = toString (
-      pkgs.writeScript "init_blocklist.sh" (import ./init_blocklist.nix { inherit pkgs config; })
+      pkgs.writeScript "init_blocklist.sh" (import ./init_blocklist.nix { inherit pkgs config mkRules; })
     );
     script = toString (
       pkgs.writeScript "blocklist_update.sh" (import ./update_blocklist.nix { inherit pkgs config; })
